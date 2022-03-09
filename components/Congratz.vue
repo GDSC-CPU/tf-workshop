b7ac0d6c04b130d1fce8cb00d3cdeb0896e38c45
Richard Michael Coo <richard.michael.coo@gmail.com>
2022-03-09 14:29:59 +0800

    finalize day 1 content

--- components/Congratz.vue
+++ components/Congratz.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+
   const props = defineProps({
     achievement: {
       default: ''
@@ -11,8 +13,14 @@
     },
     caveat: {
       default: ''
+    },
+    compact: {
+      default: false,
     }
   })
+
+
+  const marginClass = ref(props.compact ? 'mt-4' : 'mt-16')
 </script>
 
 <template>
@@ -26,7 +34,7 @@
 
   <h2>{{ secondary }}</h2>
 
-  <div class="text-gray-400 italic mt-16 text-xl">
+  <div class="text-gray-400 italic mt-16 text-xl" :class="[marginClass]">
     {{ caveat }}
   </div>
 </template>
--- slides/workshop-logistic/load-save-workshop.md
+++ slides/workshop-logistic/load-save-workshop.md
@@ -7,4 +7,12 @@ layout: fact
   message="and writing your first ML-powered app."
   secondary="In addition, you even saved and loaded a MinMaxScaler object containing min and max values!"
   caveat="Otherwise we'll have to load the training set in our app, then fit and transform 😫"
+  compact="true"
 />
+
+<h3 class="mt-16">
+  And that ends 
+  <twemoji-last-quarter-moon-face class="animate-spin" />
+  <twemoji-spiral-calendar class="animate-bounce" />
+  <b>Day <twemoji-keycap-1 class="animate-pulse" />!!</b>
+</h3>
